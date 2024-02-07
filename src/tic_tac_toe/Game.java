40f12024b6ffa5adc20cc181aa6a94f83a33f3c3
priyanka30yv <prriyanka2506@gmail.com>
2024-02-07 13:35:38 +0530

    first commit

---
+++ src/tic_tac_toe/Game.java
@@ -0,0 +1,34 @@
+package tic_tac_toe;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Game {
+	
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	 int matrix[][] = 
+	      { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	 
+	    for(int i=0; i<matrix.length; i++) {
+	  
+	      for(int j=0; j<matrix[0].length; j++) {
+	        System.out.print(matrix[i][j] + " ");
+	      }
+	      System.out.println();
+	      
+	   
+	  char a=sc.next().charAt(i);
+	  switch(a)
+	  {
+	  case 'x':
+	  }
+	    }
+	   
+
+}
+	}
+
+
+
